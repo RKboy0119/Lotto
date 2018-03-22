f48a332d9ce8ff5aae2abb433a075fd3d7268762
m305-25\w401 <zxsa33310634@gmail.com>
2018-03-22 20:26:24 +0800

    完成

--- test01/Program.cs
+++ test01/Program.cs
@@ -13,11 +13,11 @@ namespace test01
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Red;
 
+            Console.WriteLine("     (問卜)今日大樂透號碼     ");
             Random rand = new Random();
 
-            int n = 1;
-
-            while (n<10)
+            int S = 0;
+            while (true)
             {
                 int r1 = rand.Next(1, 42);
                 int r2 = rand.Next(1, 42);
@@ -26,15 +26,18 @@ namespace test01
                 int r5 = rand.Next(1, 42);
                 int r6 = rand.Next(1, 42);
 
-                Console.WriteLine(" ╔════════════╗");
-                Console.WriteLine(" ║   第"+n+"次 大樂透號碼 :   ║");
-                Console.WriteLine(" ║                        ║");
-                Console.WriteLine(" ║    {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}   ║", r1, r2, r3, r4, r5, r6);
-                Console.WriteLine(" ╚════════════╝");
+                Console.WriteLine(" ╔════════════╗ ");
+                Console.WriteLine(" ║                        ║ ");
+                Console.WriteLine(" ║      大樂透號碼 :      ║ ");
+                Console.WriteLine(" ║                        ║ ");
+                Console.WriteLine(" ║   {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}    ║ ", r1, r2, r3, r4, r5, r6);
+                Console.WriteLine(" ║                        ║ ");
+                Console.WriteLine(" ╚════════════╝ ");
 
-                n = n + 1;
+                S = S + 1;
+                Console.ReadLine();
+
             }
-
             Console.ReadLine();
         }
     }
